3a913d482b4415b3fc5821c9d2bbb1bddcafbdae
ANKUR SHARMA <ankur143513@gmail.com>
2024-12-28 00:27:20 +0530

    added some comments

--- Lec10/Insertion_Sort.java
+++ Lec10/Insertion_Sort.java
@@ -5,30 +5,24 @@ public class Insertion_Sort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = { 2, 3, 11, 12, 6, 4, 5, 8, 9 };
-		// System.out.println(Insert_Last_Element(arr, arr.length - 1));
-		sort(arr);
+		InsertionSort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 
 	}
 
-	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
-		for (int i = 1; i < arr.length; i++) {
-			Insert_Last_Element(arr, i);
-		}
-
-	}
-
-	public static void Insert_Last_Element(int[] arr, int i) {
-		int item = arr[i];
-		int j = i - 1;
-		while (j >= 0 && arr[j] > item) {
-			arr[j + 1] = arr[j];
-			arr[j] = item;
-			j--;
-		}
-
-	}
+	public static int [] InsertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            // j is the track of unsorted elements
+            int j = i-1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        return arr;
+    }
 }
--- Lec10/Selection_Sort.java
+++ Lec10/Selection_Sort.java
@@ -4,33 +4,26 @@ public class Selection_Sort {
 	public static void main(String[] args) {
 
 		int[] arr = { 4, -1, 5, 3, 1, 2 };
-		sort(arr);
+		SelectionSorting(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]+" ");
 		}
 		// System.out.println(Min_Iddex(arr, 2));
 
 	}
-
-	public static void sort(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			int mini = Min_Iddex(arr, i);
-			int temp = arr[i];
-			arr[i] = arr[mini];
-			arr[mini] = temp;
-		}
-
-	}
-
-	public static int Min_Iddex(int[] arr, int idx) {
-		int mini = idx;
-		for (int i = idx + 1; i < arr.length; i++) {
-			if (arr[i] < arr[mini]) {
-				mini = i;
-			}
-		}
-		return mini; 
-
-	}
+    public static int [] SelectionSorting(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            int smallestIdx = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[smallestIdx] > arr[j]) {
+                    smallestIdx = j;
+                }
+            }
+            int temp = arr[smallestIdx];
+            arr[smallestIdx] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
 
 }
