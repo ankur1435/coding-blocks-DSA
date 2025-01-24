8d57a747d5af6dc272756c766b60bb42bd7c03c4
ANKUR SHARMA <ankur143513@gmail.com>
2025-01-24 17:55:04 +0530

    completed the answer

--- Lec30/Middle_of_the_Linked_List.java
+++ Lec30/Middle_of_the_Linked_List.java
@@ -31,4 +31,20 @@ public class Middle_of_the_Linked_List {
 		}
 	}
 
+	public static void main(String[] args) {
+		Middle_of_the_Linked_List obj = new Middle_of_the_Linked_List();
+		Solution solution = obj.new Solution();
+
+		// Creating a linked list: 1 -> 2 -> 3 -> 4 -> 5
+		ListNode head = obj.new ListNode(1);
+		head.next = obj.new ListNode(2);
+		head.next.next = obj.new ListNode(3);
+		head.next.next.next = obj.new ListNode(4);
+		head.next.next.next.next = obj.new ListNode(5);
+
+		ListNode middle = solution.middleNode(head);
+
+		System.out.println("The middle node is: " + middle.val);
+	}
+
 }
