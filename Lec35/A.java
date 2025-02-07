2985bb5a7a1d50b56360e90b1d8cf2253144ca74
ANKUR SHARMA <ankur143513@gmail.com>
2025-02-07 17:21:15 +0530

    update in the code

--- Lec35/A.java
+++ Lec35/A.java
@@ -4,3 +4,10 @@ public interface A extends B, C {
 
 	public void Display();
 }
+
+// An interface in Java is like a contract that tells a class, "If you're going
+// to implement me, you must provide the functionality for these methods."
+
+// It only declares methods (what needs to be done) but doesn't give the actual
+// implementation (how it should be done). The class implementing the interface
+// must define how the methods work.
