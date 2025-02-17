c7347f6be9f11ac82a11e3ba22cabadf84dc0b01
ANKUR SHARMA <ankur143513@gmail.com>
2025-02-17 17:28:26 +0530

    update in code

--- 12- Stack & Queue/Lec28/Stock_Span.java
+++ 12- Stack & Queue/Lec28/Stock_Span.java
@@ -1,33 +1,29 @@
-package Lec28;
-
 import java.util.Stack;
 
 public class Stock_Span {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] arr = { 30, 35, 40, 38, 35 };
 		Cal_Span(arr);
 	}
 
-	public static void Cal_Span(int[] arr) {
+	public static void Cal_Span(int arr[]) {
 		Stack<Integer> st = new Stack<>();
-		int[] ans = new int[arr.length];
-		for (int i = 0; i < ans.length; i++) {
-			while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
-				st.pop();
-			}
-			if (st.isEmpty()) {
-				ans[i] = i + 1;
+		int ans[] = new int[arr.length];
 
-			} else {
-				ans[i] = i - st.peek();
+		for (int i = 0; i < arr.length; i++) {
+			int count = 1; // Start count for current element
+
+			// Pop elements while current price is greater
+			while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
+				count += ans[st.pop()]; // Add spans of popped elements
 			}
-			st.push(i);
+
+			ans[i] = count; // Store the span for current index
+			st.push(i); // Push current index
 		}
+
 		for (int i = 0; i < ans.length; i++) {
 			System.out.print(ans[i] + " ");
 		}
 	}
-
 }
