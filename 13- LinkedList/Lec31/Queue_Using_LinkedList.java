8dcde020200997b87fd1fc7cc21f7df1fd55adf8
ANKUR SHARMA <ankur143513@gmail.com>
2025-02-18 17:19:38 +0530

    update in linkedlist

--- 13- LinkedList/Lec31/Queue_Using_LinkedList.java
+++ 13- LinkedList/Lec31/Queue_Using_LinkedList.java
@@ -8,16 +8,25 @@ public class Queue_Using_LinkedList {
 		// TODO Auto-generated method stub
 		// LinkedList<Integer> ll = new LinkedList<>();
 		Queue<Integer> ll = new LinkedList<>();
+		// LinkedList is a class that implements the Queue interface. This means all
+		// Queue operations are already defined in LinkedList.
 		ll.add(10);// addlast--> Enqueue
 		ll.add(20);// addlast
 		ll.add(30);// addlast
 		ll.add(40);// addlast
 		ll.add(50);// addlast
 		ll.remove();// remove first--> Dequeue
-		ll.poll();// remove first-->Dequeue// it is same a remove but it returns null if the queue
+		ll.poll();// remove first-->Dequeue it is same a remove but it returns null if the queue
 					// is empty, while remove() throws an exception.
 		ll.peek();// get fornt but will not remove the element
 
 	}
 
 }
+
+// Think of it like this:
+// Queue<Integer> is like a remote control with only the buttons for queue
+// operations.
+// LinkedList<Integer> is the actual TV that stores and manages the data.
+// Data is stored in a LinkedList.
+// You access it using Queue operations.
