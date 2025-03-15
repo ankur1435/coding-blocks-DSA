aaed89ee4b48b06a325576fd0647c45358a3303f
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-15 10:06:55 +0530

    Update: Keep moving forward 🚀

--- 5- Arrays/Lec8/Arrays_Swap_1.java
+++ 5- Arrays/Lec8/Arrays_Swap_1.java
@@ -7,24 +7,29 @@ public class Arrays_Swap_1 {
 		int[] arr = { 2, 3, 1, 7, 9 };
 		// int[] arr1 = new int[]{ 2, 3, 1, 7, 9 };
 		System.out.println(arr[0] + " " + arr[1]);
-		SwapByReference(arr, 0, 1); 
+		SwapByReference(arr, 0, 1);
 		System.out.println(arr[0] + " " + arr[1]);
 	}
 
-    // call by reference
-	public static void SwapByReference(int arr[],int i, int j) {
+	// call by reference
+	public static void SwapByReference(int arr[], int i, int j) {// the array passed in the function is a reference to
+																	// the main array because in java arrays are always
+																	// passed by reference
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
 
 	// call by value
-	public static void SwapByValue(int a, int b) {
+	public static void SwapByValue(int a, int b) {// this will not work because A copy of x and y is sent to the
+													// function.
 		int temp = a;
 		a = b;
 		b = temp;
 	}
 }
 
-// Call by Value: A copy of the actual data is passed to the function, so any changes made inside the function do not affect the original data.
-// Call by Reference: The actual reference (memory address) of the data is passed, so changes made inside the function will affect the original data.
+// Call by Value: A copy of the actual data is passed to the function, so any
+// changes made inside the function do not affect the original data.
+// Call by Reference: The actual reference (memory address) of the data is
+// passed, so changes made inside the function will affect the original data.
