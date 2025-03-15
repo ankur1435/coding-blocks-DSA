e95167f5c88d0b58346cf876abe82deeeca5fefb
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-15 09:52:27 +0530

    Update: Keep moving forward 🚀

--- 5- Arrays/Lec8/Array_Demo.java
+++ 5- Arrays/Lec8/Array_Demo.java
@@ -4,8 +4,10 @@ public class Array_Demo {
 	public static void main(String[] args) {
 		int[] arr = new int[5];
 		System.out.println(arr);
-//		int a[] = new int[5];
-		int[] other = arr;
+		// int a[] = new int[5];
+		int[] other = arr; // other = arr; does not create a new array. Instead, it makes other point to
+							// the same memory location as arr.
+		System.out.println(other);
 		// set
 		arr[0] = 5;
 		arr[1] = 7;
@@ -18,7 +20,7 @@ public class Array_Demo {
 		System.out.println(arr[2]);
 		System.out.println(arr[3]);
 		System.out.println(arr[4]);
-		//System.out.println(arr[5]);
+		// System.out.println(arr[5]);
 		System.out.println(other.length);
 
 	}
