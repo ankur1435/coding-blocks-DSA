73fd8808af1efba43d95002803d72249fa0b6844
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-17 18:06:19 +0530

    1- Basics of Java

--- 5- Arrays/Lec11/Kth_root.java
+++ 5- Arrays/Lec11/Kth_root.java
@@ -1,4 +1,4 @@
-package Lec11; 
+package Lec11;
 
 public class Kth_root {
 
@@ -9,7 +9,7 @@ public class Kth_root {
 		System.out.println(KthRoot(n, k));
 
 	}
- 
+
 	public static int KthRoot(int n, int k) {
 		int lo = 1;
 		int hi = n;
@@ -17,7 +17,10 @@ public class Kth_root {
 		while (lo <= hi) {
 			int mid = (lo + hi) / 2;
 			if (Math.pow(mid, k) <= n) {
-				// we are updating ans here and modifying lo because if we find this if check true but we need the highest one who can satisfy this if check and always thing this will not go in an infinte loop because somewhere lo will be higher than hi and loop will exit 
+				// we are updating ans here and modifying lo because if we find this if check
+				// true but we need the highest one who can satisfy this if check and always
+				// thing this will not go in an infinte loop because somewhere lo will be higher
+				// than hi and loop will exit
 				ans = mid;
 				lo = mid + 1;
 			} else {
--- 5- Arrays/Lec12/Spiral_print.java
+++ 5- Arrays/Lec12/Spiral_print.java
@@ -10,7 +10,7 @@ public class Spiral_print {
 
 	public static void print(int[][] arr) {
 		int minr = 0, minc = 0, maxr = arr.length - 1, maxc = arr[0].length - 1;
-		int te = arr.length * arr[0].length;
+		int te = arr.length * arr[0].length;// total number of elements in the array
 		int c = 0;
 		while (c < te) {
 			for (int i = minc; i <= maxc && c < te; i++) {
@@ -21,7 +21,7 @@ public class Spiral_print {
 			for (int i = minr; i <= maxr && c < te; i++) {
 				System.out.print(arr[i][maxc] + " ");
 				c++;
- 			}
+			}
 			maxc--;
 			for (int i = maxc; i >= minc && c < te; i--) {
 				System.out.print(arr[maxr][i] + " ");
@@ -37,6 +37,10 @@ public class Spiral_print {
 	}
 }
 
-// Without c < te inside each loop, these loops might over run their boundaries and attempt to process elements unnecessarily (even though the while loop has the main condition).
+// Without c < te inside each loop, these loops might over run their boundaries
+// and attempt to process elements unnecessarily (even though the while loop has
+// the main condition).
 
-// Removing c < te from the for loops might work for most cases, but it introduces a risk of bugs in Edge Cases (e.g., single row/column arrays or irregular dimensions)
+// Removing c < te from the for loops might work for most cases, but it
+// introduces a risk of bugs in Edge Cases (e.g., single row/column arrays or
+// irregular dimensions)
