a415b261de6847d7ce45d9d71143687d0acf084a
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-23 23:12:35 +0530

    1- Basics of Java

--- 6- Sorting Algorithms/Lec10/Selection_Sort.java
+++ 6- Sorting Algorithms/Lec10/Selection_Sort.java
@@ -1,20 +1,21 @@
 package Lec10;
 
 public class Selection_Sort {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] arr = { 4, -1, 5, 3, 1, 2 };
-		SelectionSorting(arr);
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]+" ");
-		}
-		// System.out.println(Min_Iddex(arr, 2));
+        int[] arr = { 4, -1, 5, 3, 1, 2 };
+        SelectionSorting(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        // System.out.println(Min_Iddex(arr, 2)); //
 
-	}
-    public static int [] SelectionSorting(int arr[]){
-        for (int i = 0; i < arr.length-1; i++) {
+    }
+
+    public static int[] SelectionSorting(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int smallestIdx = i;
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[smallestIdx] > arr[j]) {
                     smallestIdx = j;
                 }
