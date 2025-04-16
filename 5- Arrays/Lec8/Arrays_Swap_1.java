010c955e268a7d883fc4727150fdfcfa3bfbda60
ANKUR SHARMA <ankur143513@gmail.com>
2025-04-16 15:18:51 +0530

    Update in 5- Arrays/Lec8/Arrays_Swap_1.java

--- 5- Arrays/Lec8/Arrays_Swap_1.java
+++ 5- Arrays/Lec8/Arrays_Swap_1.java
@@ -6,6 +6,7 @@ public class Arrays_Swap_1 {
 		// TODO Auto-generated method stub
 		int[] arr = { 2, 3, 1, 7, 9 };
 		// int[] arr1 = new int[]{ 2, 3, 1, 7, 9 };
+		// Java is call by value for primitive types.
 		System.out.println(arr[0] + " " + arr[1]);
 		SwapByReference(arr, 0, 1);
 		System.out.println(arr[0] + " " + arr[1]);
@@ -33,3 +34,32 @@ public class Arrays_Swap_1 {
 // changes made inside the function do not affect the original data.
 // Call by Reference: The actual reference (memory address) of the data is
 // passed, so changes made inside the function will affect the original data.
+
+// you can swap primitive like int and float etc by making and object of a class
+// like:
+
+// class IntWrapper {
+// int value;
+
+// IntWrapper(int value) {
+// this.value = value;
+// }
+// }
+
+// public class SwapWithWrapper {
+// public static void main(String[] args) {
+// IntWrapper a = new IntWrapper(10);
+// IntWrapper b = new IntWrapper(20);
+
+// swap(a, b);
+
+// System.out.println("a = " + a.value + ", b = " + b.value); // Output: a = 20,
+// b = 10
+// }
+
+// public static void swap(IntWrapper x, IntWrapper y) {
+// int temp = x.value;
+// x.value = y.value;
+// y.value = temp;
+// }
+// }
