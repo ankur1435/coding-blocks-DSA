1f96d4989f59167f4301ab4e0d92b191db3bcbe3
ANKUR SHARMA <ankur143513@gmail.com>
2025-04-18 17:51:52 +0530

    Update in 8- Strings/Lec14/Check_Palindromic.java

--- 8- Strings/Lec14/Check_Palindromic.java
+++ 8- Strings/Lec14/Check_Palindromic.java
@@ -12,8 +12,8 @@ public class Check_Palindromic {
 	public static boolean isPalindrom(String s) {
 		int i = 0;
 		int j = s.length() - 1;
-		while(i<j) {
-			if(s.charAt(i) != s.charAt(j)) {
+		while (i < j) {
+			if (s.charAt(i) != s.charAt(j)) {
 				return false;
 			}
 			i++;
--- 8- Strings/Lec14/String_Demo.java
+++ 8- Strings/Lec14/String_Demo.java
@@ -3,17 +3,19 @@ package Lec14;
 public class String_Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		// TODO Auto-generated method stub
 		String s1 = "hello";
-		String s2 = "hello";
-		String s3 = new String("hello");
+		String s2 = "hello";// if there is already a string which have value hello then this one will not be
+							// created will reuse that one only
+		String s3 = new String("hello");// always creates a new one if already there with same value and this store the
+										// string in heap.
 		String s4 = new String("hello");
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
 		System.out.println(s4);
-		s1 = s1 + "bye"; //s1.concat("bye");
-		String s6="Hello"+"Bye";
+		s1 = s1 + "bye"; // s1.concat("bye");
+		String s6 = "Hello" + "Bye";
 		System.out.println(s6);
 
 	}
