7289d8ba71d004c4b9ad8db9bdb6d0ef499d334a
ANKUR SHARMA <ankur143513@gmail.com>
2025-05-22 13:46:05 +0530

    Update 6- Sorting Algorithms/Lec24/Power_Log_N.java

--- 6- Sorting Algorithms/Lec24/Random_Number.java
+++ 6- Sorting Algorithms/Lec24/Random_Number.java
@@ -10,7 +10,7 @@ public class Random_Number {
 		int ei = 100;
 		Random rn = new Random();
 		for (int i = 0; i < 10; i++) {
-			int x = rn.nextInt(ei - si + 1) + si;
+			int x = rn.nextInt(ei - si + 1) + si;// his will generate a random integer from 0 to 99 (not 100!).
 			System.out.println(x);
 		}
 
